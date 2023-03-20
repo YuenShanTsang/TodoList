2a6be7006ecf657ef75500f8042287159919e812
YuenShanTsang <A00270323@mycambrian.ca>
2023-03-20 17:10:09 -0400

    Completed the edit text, check box, add and remove button function by creating onClickListener.

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.todolist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.databinding.TodoBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class TodoAdapter(context: Context, tasks: List<String>) :
+    ArrayAdapter<String>(context, R.layout.todo, R.id.task_text_view, tasks) {
+
+    private val inflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding: TodoBinding
+        val view = convertView ?: inflater.inflate(R.layout.todo, parent, false)
+        binding = TodoBinding.bind(view)
+
+        binding.taskTextView.text = getItem(position)
+
+        binding.removeButton.setOnClickListener {
+            remove(getItem(position))
+        }
+
+        return view
     }
 }
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: TodoAdapter
+
+    private var tasks = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = TodoAdapter(this, tasks)
+        binding.todoListView.adapter = adapter
+
+        binding.addButton.setOnClickListener {
+            val task = binding.inputEditText.text.toString().trim()
+            if (task.isNotEmpty()) {
+                tasks.add(task)
+                adapter.notifyDataSetChanged()
+                binding.inputEditText.text.clear()
+            }
+        }
+    }
+}
+
+
